54010a84a9cc2dcc0ab4896f1241917dda05a3f3
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2023-02-24 07:50:01 +0000

    force authentication

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Head from "next/head"
-import { Inter } from "next/font/google"
-
-const inter = Inter({ subsets: ["latin"] })
+import { withPageAuthRequired } from "@auth0/nextjs-auth0"
+import { useUser } from "@auth0/nextjs-auth0/client"
 
 export default function Home() {
+  const { user, error, isLoading }: any = useUser()
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
   return (
     <>
       <Head>
@@ -12,11 +16,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <main>
+      <main className="p-5">
         <h1 className="text-3xl font-bold underline">
-          Welcome to ProgressTeaching
+          Hi {user.name}, welcome to ProgressTeaching!
         </h1>
       </main>
     </>
   )
 }
+
+export const getServerSideProps = withPageAuthRequired()
